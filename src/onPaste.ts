36bb0fb5b11ea4195573bbbe9ec56894a0caf555
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-05-29 01:06:19 -0400

    allow pasting data url as raw text

--- src/onPaste.ts
+++ src/onPaste.ts
@@ -1,7 +1,13 @@
 export function onPasteImage(callback: (image: string) => void) {
 	document.addEventListener('paste', (event) => {
-		const items = (event.clipboardData || event.originalEvent.clipboardData)
-			.items;
+		const data = event.clipboardData || event.originalEvent.clipboardData;
+		const text = data.getData('text');
+		if (text?.startsWith('data:image')) {
+			callback(text);
+			return;
+		}
+
+		const items = data.items;
 		for (let index in items) {
 			const item = items[index];
 			if (item.kind === 'file') {
